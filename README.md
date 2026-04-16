1072a8fccdee693499e02c47bb6a0e5d7d6227f0
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-04-17 06:53:17 +0900

    i need to take a break

--- README.md
+++ README.md
@@ -8,11 +8,13 @@ Dead end: 14/04/2026 started, after almost 2 month
 use theWatcher::TheWatcher;
 
 fn main() ->  -> Result<(), Box<dyn std::error::Error>> {
+  let pid= 111;
+  let output_path= "./dir";
 
-  let mut watcherA= TheWatcher::new(pid);
+  let mut watcherA= TheWatcher::new(pid, output_path);
   watcherA.setting_target()
           .logging(true, TheWatcher::LoggingOptions::ALL)
-          .output_path("./log.txt")
+          .output_txt_path()?
           .csv_format_option(true)?;
 
   Ok(())
@@ -40,6 +42,10 @@ step-by-step
 <img width="901" height="641" alt="0303_14-04-26-enum" src="https://github.com/user-attachments/assets/a7883df5-e9c7-4599-a8ca-ac76d32e69c7" />
 
 by Google's Gemini
+
+# fn output_txt_path
+- Default path: "./"
+
 
 # csv_format_option(@TODO)
 - true: save a log file as Comma-separated values
--- src/theWatcher/mod.rs
+++ src/theWatcher/mod.rs
@@ -1,24 +1,61 @@
+use std::{error::Error, io, process::Stdio};
+
+pub enum LoggingOptions {
+    ALL,
+    KEYBOARD_ONLY,
+    MOUSE_ONLY,
+    NETWORK_ACTIVITY_MODE
+}
+
 
 struct TheWatcher{
     pid: i32,
     logging_flag: bool,
     output_path: &'static str,
     csv_option: bool,
-    // target: process::
+    // target: process::Something ,
+    // data_bus_steam: Someting,
 }
 
 impl TheWatcher{
-    pub fn new(pid: i32) -> Self{
+    pub fn new(pid: i32, output_path: &'static str) -> Self{
         let logging_flag= true;
-        let output_path= "";
         let csv_option= false;
         // let target= process::new()
-         
+        // let data_bus_steam= something;
+
         Self{ pid, logging_flag, output_path, csv_option }
     }
 
     fn setting_target(&mut self) -> &mut Self{
         // hook a target
+        // self.target= SomeTool::hook(pid);
 
+        self
+    }
+
+    fn logging(&mut self, 
+                flag: bool, 
+                option: TheWatcher::LoggingOptions) 
+                -> &mut Self{
+                    
+        // logging
+        // @TODO
+
+        self.logging_flag= flag;
+        self.option= option;
+        
+        self
+    }
+
+    fn output_txt_path(&mut self) -> Result<&mut Self, Error>{
+        if self.output_path == "" {
+            self.output_path= "./"
+        }
+
+        // Keep writing data bus steam 
+        // io::Write
+
+        Ok(self)
     }
 }
