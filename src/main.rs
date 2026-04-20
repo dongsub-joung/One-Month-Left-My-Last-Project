1a400cd944fa789c6a322893d6a379f3130cbbf7
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2026-04-20 04:04:10 +0000

    Add fn) logging dependency fn

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
+use the_watcher::*;
+
 use anyhow::Result;
 
 mod the_watcher;
-// mod the_Watcher;
 
-use the_watcher::*;
+#[tokio::main]
+async fn main() -> Result<()> {
+    let pid = 111;
+    let output_path = "./dir";
 
-fn main() -> Result<()>{
-  let pid= 111;
-  let output_path= "./dir";
+    let mut watcher_a = TheWatcher::new(pid, output_path);
+    watcher_a
+        .setting_target()
+        .logging(true, LoggingOptions::ALL)
+        .await
+        .output_txt_path()?
+        .csv_format_option(true)?;
 
-  let mut watcher_a= TheWatcher::new(pid, output_path);
-  watcher_a.setting_target()
-          .logging(true, LoggingOptions::ALL)
-          .output_txt_path()?
-          .csv_format_option(true)?;
-
-  Ok(())
+    Ok(())
 }
--- src/the_watcher/mod.rs
+++ src/the_watcher/mod.rs
@@ -5,11 +5,10 @@ pub enum LoggingOptions {
     ALL,
     KEYBOARD_ONLY,
     MOUSE_ONLY,
-    NETWORK_ACTIVITY_MODE
+    NETWORK_ACTIVITY_MODE,
 }
 
-
-pub struct TheWatcher{
+pub struct TheWatcher {
     pid: i32,
     logging_flag: bool,
     output_path: &'static str,
@@ -19,20 +18,26 @@ pub struct TheWatcher{
     // data_bus_steam: Someting,
 }
 
-impl TheWatcher{
-    pub fn new(pid: i32, output_path: &'static str) -> Self{
-        let logging_flag= true;
-        let csv_option= false;
+impl TheWatcher {
+    pub fn new(pid: i32, output_path: &'static str) -> Self {
+        let logging_flag = true;
+        let csv_option = false;
         let option = LoggingOptions::ALL;
         // let target= process::new()
         // let data_bus_steam= something;
 
-        Self{ pid, logging_flag, output_path, csv_option, option } // target, data_bus_steam
+        Self {
+            pid,
+            logging_flag,
+            output_path,
+            csv_option,
+            option,
+        } // target, data_bus_steam
     }
 
-    pub fn setting_target(&mut self) -> &mut Self{
+    pub fn setting_target(&mut self) -> &mut Self {
         // let target= process::new()
-        
+
         // hook a target
         // self.target= SomeTool::hook(pid);
         // self.data_bus_steam= something;
@@ -40,32 +45,43 @@ impl TheWatcher{
         self
     }
 
-    pub fn logging(&mut self, 
-                flag: bool, 
-                option: LoggingOptions) 
-                -> &mut Self{
-                    
-        self.logging_flag= flag;
-        self.option= option;
+    async fn read_data_steam(data_bus_steam: steam) -> Result<Box<Vec<usize>>>{
+        let mut buffer: Vec<usize>= Vec::new();
+       
+        // @TODO if returned err, try to reconn
+        buffer= io::read(data_bus_steam)?;
+
+        if (buffer.capacity) >= 1024000000{
+            return Ok(Box::from(buffer));
+        }
+
+        Ok(Box::new(Vec::new()))
+    }
+    async pub fn logging(&mut self, flag: bool, option: LoggingOptions) -> &mut Self {
+        self.logging_flag = flag;
+        self.option = option;
+
         
         // logging
-        // @TODO
+        let data= read_data_steam(self.data_bus_steam);
+         
 
         self
     }
 
-    pub fn output_txt_path(&mut self) -> Result<&mut Self>{
+    pub fn output_txt_path(&mut self) -> Result<&mut Self> {
         if self.output_path == "" {
-            self.output_path= "./"
+            self.output_path = "./"
         }
 
-        // Keep writing data bus steam 
+
+
+        // Keep writing data bus steam
         // io::Write
         Ok(self)
     }
 
-    pub fn csv_format_option(&mut self, flag: bool) -> Result<&mut Self>{
-
+    pub fn csv_format_option(&mut self, flag: bool) -> Result<&mut Self> {
         Ok(self)
     }
 }
