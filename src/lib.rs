3d3ec2d43bea3cc076f1f42fae0da3458498ec44
DongSub_Joung <ehdtjqwjd@kangwon.ac.kr>
2026-04-22 00:55:53 +0900

    Unit Test) fn get_process_name is work

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,30 @@
 pub mod the_watcher;
 
-#[macro_use]
-extern crate assert_type_eq;
-
 #[cfg(test)]
 mod tests {
+    use anyhow::Error;
+
     use crate::the_watcher::TheWatcher;
-
     use super::*;
-    use the_watcher;
 
+//  thread 'tests::test_get_process_name' (15388) panicked at src\lib.rs:16:21:
+//  assertion `left == right` failed
+//  left: "C:\\Program Files (x86)\\Steam\\steamapps\\common\\Grisaia Chronos Rebellion\\GCR.exe"
+//  right: "value"
+//  note: run with `RUST_BACKTRACE=1` environment variable to display a backtrace
+//  test tests::test_get_process_name ... FAILED
     #[test]
-    fn get_process_name() {
-        let mut result;
+    fn test_get_process_name() {
         unsafe {
-            result = TheWatcher::get_name_process(12);
+            let _result = TheWatcher::get_name_process(24264);
+            match _result {
+                Ok(_string) =>{
+                    assert_eq!(_string, String::from("value"));
+                },
+                Err(e) =>{
+                    eprint!("{}", e);
+                }
+            }
         }
-        let mut result_string;
-        assert_type_eq!(result, windows::core::Result<String>);
     }
 }
